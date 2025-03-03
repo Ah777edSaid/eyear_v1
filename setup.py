83f7cdf0fc52dad4d7de75073a536610c4a250fd
Ah777ed_said <ahmed01226313482@gmail.com>
2025-03-03 20:28:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='eyear',
+    version='0.1.0',
+    description='eyear.',
+    packages=find_packages(),
+    install_requires=[],
+)
